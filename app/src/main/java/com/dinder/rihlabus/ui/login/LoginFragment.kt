ea35f31f9fefc723b112e1a5a1b1af52b31c1359
Ahmedgadein <ahmed.g.obied@gmail.com>
2022-03-05 11:47:56 +0200

    login screen UI

--- app/src/main/java/com/dinder/rihlabus/ui/login/LoginFragment.kt
+++ app/src/main/java/com/dinder/rihlabus/ui/login/LoginFragment.kt
@@ -5,28 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.dinder.rihlabus.R
+import com.dinder.rihlabus.databinding.LoginFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class LoginFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = LoginFragment()
-    }
-
-    private val viewModel:LoginViewModel by viewModels()
+    private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.login_fragment, container, false)
+        val binding = LoginFragmentBinding.inflate(inflater, container, false)
+        binding.signupButton.setOnClickListener {
+            navigateToSignup()
+        }
+
+        return binding.root
+    }
+
+    private fun navigateToSignup() {
+        val action = LoginFragmentDirections.actionLoginFragmentToSignupFragment()
+        findNavController().navigate(action)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
     }
-
 }
