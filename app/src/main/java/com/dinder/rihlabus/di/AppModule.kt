3bd77ef5212dea422073e76df96dbda68b7782b8
Ahmedgadein <ahmed.g.obied@gmail.com>
2022-03-22 00:05:47 +0200

    main fragment starting point

--- app/src/main/java/com/dinder/rihlabus/di/AppModule.kt
+++ app/src/main/java/com/dinder/rihlabus/di/AppModule.kt
@@ -7,12 +7,16 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @Module
 @InstallIn(SingletonComponent::class)
 @ExperimentalCoroutinesApi
-object AuthModule {
+object AppModule {
+    @Provides
+    fun getIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
     @Provides
     fun getAuthRepository(
         ioDispatcher: CoroutineDispatcher
---
+++ app/src/main/java/com/dinder/rihlabus/ui/home/currentTrips/CurrentTripsViewModel.kt
@@ -0,0 +1,7 @@
+package com.dinder.rihlabus.ui.home.currentTrips
+
+import androidx.lifecycle.ViewModel
+
+class CurrentTripsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
