1cf3325a95cfb1cfc959282d80d0e94364985c23
Ahmedgadein <ahmed.g.obied@gmail.com>
2022-03-21 18:36:53 +0200

    validators and formatters tests

---
+++ app/src/test/java/com/dinder/rihlabus/PhoneValidatorTest.kt
@@ -0,0 +1,39 @@
+package com.dinder.rihlabus
+
+import com.dinder.rihlabus.utils.PhoneNumberValidator
+import junit.framework.Assert.assertNotNull
+import junit.framework.Assert.assertNull
+import org.junit.Test
+import kotlin.random.Random
+
+class PhoneValidatorTest {
+    private val validNumbers = listOf("123456789", "912345678")
+
+    @Test
+    fun phoneValidator_CorrectNumber() {
+        assertNull(PhoneNumberValidator.validate("901145980"))
+    }
+    @Test
+    fun phoneValidator_EmptyOrNullNumber() {
+        assertNotNull(PhoneNumberValidator.validate(""))
+    }
+
+    @Test
+    fun phoneValidator_IncorrectNumberFormat() {
+        /* Accepted formats are:
+            [+249] 9xxxxxxxx
+            [+249] 1xxxxxxxx
+         */
+        assertNotNull(PhoneNumberValidator.validate("23456789"))
+    }
+
+    @Test
+    fun phoneValidator_IncorrectShortNumber() {
+        assertNotNull(PhoneNumberValidator.validate("1234567"))
+    }
+
+    @Test
+    fun phoneValidator_IncorrectLongNumber() {
+        assertNotNull(validNumbers[Random.nextInt(validNumbers.size)])
+    }
+}
